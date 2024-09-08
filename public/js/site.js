e4c49f57f020d0236edc232b040dd388d4f92427
Brandon Ibach <brandon.l.ibach@gmail.com>
2024-09-08 06:12:42 -0400

    Add News section with links and previews of last 3 blog entries.

--- public/js/site.js
+++ public/js/site.js
@@ -14,11 +14,40 @@ function toggleDetail(e) {
     }
 }
 
+function loadGoogleApi(url, callback) {
+    var API_URL = 'https://www.googleapis.com/';
+    var API_KEY = '&key=AIzaSyDQQv6yLc_d3zkHDDHHH6j43N9iZzKQLEA';
+    var req = new XMLHttpRequest();
+    req.addEventListener('load', callback);
+    req.open('GET', API_URL + url + API_KEY);
+    req.send();
+}
+
+function loadBlogFeed() {
+    var BLOG_URL = 'blogger/v3/blogs/';
+    var BLOG_ID = '4570358893544606507';
+    var BLOG_ARGS = '/posts?status=live&orderBy=updated&maxResults=3&view=READER&fetchImages=true&fetchBodies=true';
+    loadGoogleApi(BLOG_URL + BLOG_ID + BLOG_ARGS, function() {
+        var postsDiv = document.getElementById('posts');
+        var posts = JSON.parse(this.response).items;
+        var rows = '';
+        for (var i = 0; i < posts.length; i++) {
+            var post = posts[i];
+            var img_src = post.images.length ? post.images[0].url : '';
+            img_src = (img_src || '').replace(/\/[whs][0-9]+[-=,a-zA-F0-9]*\/([^\/]+)$/, '/w72-h72-p-k-no-nu/$1');
+            var summary = (post.content || '').replace(/<[^>]+>/g, ' ').replace(/[ \t\r\n]+/g, ' ').trim();
+            summary = summary.replace(/(.{180,200}[^ .]*) .*/, '$1\u2026');
+            rows += '<p><a href="' + post.url + '"><img src="' + img_src + '"/></a>' +
+                       '<a href="' + post.url + '">' + post.title + '</a><br/>' + summary + '</p>'
+        }
+        postsDiv.innerHTML = rows == '' ? 'No blog posts.' : rows;
+    });
+}
+
 function loadEvents() {
-    var CAL_URL = 'https://www.googleapis.com/calendar/v3/calendars/';
+    var CAL_URL = 'calendar/v3/calendars/';
     var CAL_ID = '6d94a4fc92555fbdd3a2541300bfa9ce8a55621d1cd9feafd12aeb18fc489e90%40group.calendar.google.com';
     var CAL_ARGS = '/events?maxResults=20&orderBy=startTime&singleEvents=true';
-    var API_KEY = '&key=AIzaSyDQQv6yLc_d3zkHDDHHH6j43N9iZzKQLEA';
     var ADD_URL = 'https://www.google.com/calendar/render?action=TEMPLATE&sf=true&output=xml&sprop=website:spacecoasteva.club';
     var MAP_URL = 'https://maps.google.com/maps?q=';
     var lang = navigator.languages[0];
@@ -26,8 +55,7 @@ function loadEvents() {
     var mtz = Intl.DateTimeFormat().resolvedOptions().timeZone != etz.timeZone ? ' ET' : '';
     var min = '&timeMin=' + (new Date(Date.now() - 182 * 24 * 60 * 60 * 1000)).toISOString();
     var max = '&timeMax=' + (new Date(Date.now() + 62 * 24 * 60 * 60 * 1000)).toISOString();
-    var req = new XMLHttpRequest();
-    req.addEventListener('load', function() {
+    loadGoogleApi(CAL_URL + CAL_ID + CAL_ARGS + min + max, function() {
         var eventsDiv = document.getElementById('events');
         var prevDiv = document.getElementById('prev-events');
         var events = JSON.parse(this.response).items;
@@ -99,8 +127,11 @@ function loadEvents() {
         prevDiv.innerHTML = prevRows == '' ? 'No previous events.' : prevRows;
         document.body.addEventListener('click', toggleDetail);
     });
-    req.open('GET', CAL_URL + CAL_ID + CAL_ARGS + min + max + API_KEY);
-    req.send();
 }
 
-window.addEventListener("load", loadEvents, false);
+function loadContent() {
+    loadEvents();
+    loadBlogFeed();
+}
+
+window.addEventListener("load", loadContent, false);
